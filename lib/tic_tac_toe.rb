39f5bb05eea1c9ff39a0d8ea94110d313b36fcd5
Adam Clayman <adam@clayman.org>
2017-10-04 15:27:50 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,6 @@
 class TicTacToe
   def initialize
-    @board = Array.new([" ", " ", " ", " ", " ", " ", " ", " ", " "])
+    @board = board || Array.new(9, " ")
   end
   def input_to_index(user_input)
     user_input.to_i - 1
